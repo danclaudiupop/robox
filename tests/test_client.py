1f5ae552781df412d568ec887200fbabe5cf9437
Dan Claudiu Pop <danclaudiupop@gmail.com>
2022-02-05 10:16:28 +0200

    add test for cache

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,16 +2,16 @@ import httpx
 import pytest
 import respx
 
-from robox import AsyncRobox, Robox
+from robox import AsyncRobox, DictCache, Robox
 
 TEST_URL = "https://foo.bar"
 
 
 def test_open(respx_mock):
     respx_mock.get(TEST_URL).respond(200)
-    robox = Robox()
-    page = robox.open(TEST_URL)
-    assert page.status_code == 200
+    with Robox() as robox:
+        page = robox.open(TEST_URL)
+        assert page.status_code == 200
 
 
 @pytest.mark.asyncio
@@ -24,24 +24,24 @@ async def test_async_open(respx_mock):
 
 def test_refresh(respx_mock):
     respx_mock.get(TEST_URL).respond(200)
-    robox = Robox()
-    robox.open(TEST_URL)
-    assert robox.total_requests == 1
-    robox.refresh()
-    assert robox.total_requests == 2
-    assert len(robox.get_history()) == 1
+    with Robox() as robox:
+        robox.open(TEST_URL)
+        assert robox.total_requests == 1
+        robox.refresh()
+        assert robox.total_requests == 2
+        assert len(robox.get_history()) == 1
 
 
 def test_back_without_history():
     with pytest.raises(ValueError):
-        robox = Robox()
-        robox.back()
+        with Robox() as robox:
+            robox.back()
 
 
 def test_forward_without_history():
     with pytest.raises(ValueError):
-        robox = Robox()
-        robox.forward()
+        with Robox() as robox:
+            robox.forward()
 
 
 @respx.mock(base_url=TEST_URL)
@@ -50,21 +50,21 @@ def test_back_and_forward(respx_mock):
     second_url = f"{TEST_URL}/2"
     respx_mock.get("/1").respond(200)
     respx_mock.get("/2").respond(200)
-    robox = Robox()
-    robox.open(first_url)
-    robox.open(second_url)
-    robox.back()
-    robox.current_url == first_url
-    robox.forward()
-    robox.current_url == second_url
+    with Robox() as robox:
+        robox.open(first_url)
+        robox.open(second_url)
+        robox.back()
+        robox.current_url == first_url
+        robox.forward()
+        robox.current_url == second_url
 
 
 def test_download(respx_mock, tmpdir):
     download_url = f"{TEST_URL}/foo.bin"
     respx_mock.get(download_url).respond(200, text="Foo")
-    robox = Robox()
-    robox.download_file(url=download_url, destination_folder=tmpdir)
-    assert (tmpdir / "foo.bin").exists()
+    with Robox() as robox:
+        robox.download_file(url=download_url, destination_folder=tmpdir)
+        assert (tmpdir / "foo.bin").exists()
 
 
 @pytest.mark.asyncio
@@ -80,3 +80,12 @@ def test_raise_on_4xx_5xx(respx_mock):
     respx_mock.get(TEST_URL).respond(400)
     with pytest.raises(httpx.HTTPStatusError):
         Robox(raise_on_4xx_5xx=True).open(TEST_URL)
+
+
+def test_cache(respx_mock):
+    respx_mock.get(TEST_URL).respond(200, html="<html>foo</html>")
+    with Robox(cache=DictCache()) as robox:
+        p1 = robox.get(TEST_URL)
+        assert not p1.from_cache
+        p2 = robox.get(TEST_URL)
+        assert p2.from_cache
